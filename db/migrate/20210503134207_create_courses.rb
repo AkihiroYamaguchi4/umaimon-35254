80505fdcd09b421352b91901236d63545708bd71
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-05-03 22:48:01 +0900

    couseモデル

---
+++ db/migrate/20210503134207_create_courses.rb
@@ -0,0 +1,13 @@
+class CreateCourses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :courses do |t|
+      t.string :course_title, null: false
+      t.string :description,  null: false
+      t.string :price,        null: false
+      t.string :start_date,   null: false
+      t.string :end_date,     null: false
+      t.references :chef,     null: false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
