42e2ec0bd3d2aed217b10e88c726ebea5dc8de7a
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-05-03 19:23:01 +0900

    chefモデル単体テスト

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,12 +6,14 @@ class Chef < ApplicationRecord
          with_options presence: true do
           validates :chef_nickname
           validates :chef_birth_id
-          validates :chef_career
           validates :belong
+          validates :chef_career
           validates :chef_phone_number, format: { with: /\A\d{11}\z/, message: '11桁の半角数字で入力してください' }
           with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' } do
             validates :chef_family_name
             validates :chef_first_name
           end
+          PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i.freeze
+          validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには半角英字と半角数字の両方を含めて設定してください'
         end
  end
--- spec/factories/chefs.rb
+++ spec/factories/chefs.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :chef do
-    
+    chef_nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password { 'a1' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    chef_family_name { '佐藤' }
+    chef_first_name { '太郎' }
+    chef_birth_id { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
+    belong { 'ホテル' }
+    chef_career { 'シェフとして勤務' }
+    chef_phone_number { '08011111111' }
+    after(:build) do |chef|
+      chef.avatar.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
