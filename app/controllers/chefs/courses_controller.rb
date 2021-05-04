0a5d3122e0454187ecf05d6a197544118075da7b
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-05-04 18:06:08 +0900

    courseコントローラールーティング

---
+++ app/controllers/chefs/courses_controller.rb
@@ -0,0 +1,10 @@
+class CoursesController < Chefs::ApplicationController
+ def index
+ end
+
+ def new
+ end
+
+ def create
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
     passwords:     'chefs/passwords',
     registrations: 'chefs/registrations'
   }
+  namespace :chefs do
+    resources :courses, only: [:index , :new, :create]
+  end
+
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
-  
+ 
 end
