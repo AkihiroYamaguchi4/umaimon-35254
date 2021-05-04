10130fb4162988cc49cf3f04110655cb5ddcadd6
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-05-04 19:00:55 +0900

    コース新規登録画面

--- app/controllers/chefs/courses_controller.rb
+++ app/controllers/chefs/courses_controller.rb
@@ -1,10 +1,26 @@
-class CoursesController < Chefs::ApplicationController
- def index
- end
+class Chefs::CoursesController < ApplicationController
+  def index
+    @chefs_courses = Course.all
+  end
+  
 
  def new
+  @course = Course.new
  end
 
  def create
+  @course = Course.new(course_params)
+  if @course.save
+    redirect_to root_path
+  else
+    render :new
+  end
+ end
+
+ private
+
+ def course_params
+  params.require(:course).permit(:course_title, :description, :price, :start_date, :end_date, :images[]).merge(chef_id: current_chef.id)
+
  end
 end
