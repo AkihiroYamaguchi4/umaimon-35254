63b76418c3a613f574821a279e1208a44f3fb673
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-05-07 17:59:37 +0900

    バリテーション変更１

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,7 @@ class Course < ApplicationRecord
   with_options presence: true do
     validates :course_title
     validates :description
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字で入力して下さい' }
     validates :start_datetime
     validates :end_datetime
     validates :course_images
@@ -15,13 +15,13 @@ class Course < ApplicationRecord
   
   def start_check
      unless start_datetime == nil 
-            errors.add(:start_datetime, "は現在の日時より遅い時間を選択してください") if self.start_datetime < DateTime.now
+            errors.add(:start_datetime, "開始時刻は現在の日時より遅い時間を選択してください") if self.start_datetime < DateTime.now
     end
   end
 
   def start_end_check
     unless end_datetime == nil 
-      errors.add(:end_datetime, "は開始時刻より遅い時間を選択してください") if self.start_datetime > self.end_datetime
+      errors.add(:end_datetime, "終了時刻は開始時刻より遅い時間を選択してください") if self.start_datetime > self.end_datetime
     end
   end
   
